08802b4f3c5eb090a6383e5f699f27dbf0904a60
Daniel Marchena Parreira <danielmapar@gmail.com>
2021-11-27 02:32:32 -0500

    bits_reversed - solved

--- EPIJudge/epi_judge_cpp/reverse_bits.cc
+++ EPIJudge/epi_judge_cpp/reverse_bits.cc
@@ -1,7 +1,27 @@
 #include "test_framework/generic_test.h"
 unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+
+  unsigned long long result = 0UL;
+  short bit = 0UL;
+  // (sizeof = number of bytes) * (CHAR_BIT = 8 bits)
+  short numOfBits = sizeof(result) * CHAR_BIT; 
+
+  // O(n) --> n = size of var in bits
+  while (numOfBits > 1) {
+    bit = x & 1UL;
+
+    result = result ^ bit;
+    result <<= 1;
+
+    x >>= 1;
+
+    numOfBits--;
+  }
+
+  // Another option is to extract the left most bit
+  // i & (1 << (sizeof(unsigned int) * CHAR_BIT - 1))
+
+  return result;
 }
 
 int main(int argc, char* argv[]) {
